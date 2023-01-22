ee7e09a8b3e59033282b606d2d70effa43679e1f
Christian Lancelot <cmlancelot@gmail.com>
2023-01-22 18:41:09 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const navbar = (
+  <nav>
+    <h1>website</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+ReactDOM.render(
+  navbar,
+  document.getElementById("root")
+)
